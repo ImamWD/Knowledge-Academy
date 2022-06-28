ab16720b2f255213ed153994b21c43f2df644ebb
Imam_Ib <imammosa537@gmail.com>
2022-06-29 02:30:15 +0300

    Update for Courses

---
+++ Knowledge Academy/Knowledge Academy/js/All_Courses.js
@@ -0,0 +1,50 @@
+let AllCourses = document.getElementById('All-Courses');
+let Courses = [];
+if(JSON.parse(localStorage.getItem("Courses")) == null)
+{
+     Courses=[];
+}
+else {
+    Courses = JSON.parse(localStorage.getItem("Courses"));
+    Display();
+}
+Display();
+
+function Display()
+{
+    let val =``;
+    for(let i=0;i<Courses.length;i++)
+    {
+        val +=
+        `
+        <div class="show-course" style="width:23%;">
+        <img src="imgs/courses-image/${Courses[i].Image}" style="width:100% ; margin:0px 0px ; border-top-left-radius: .75rem; border-top-right-radius: .75rem;">
+        <div style="margin-top:10px ; margin-left:10px ;">
+            <h4>${Courses[i].Name} Course</h4>
+            <p><i class="fa-solid fa-clock" ></i> ${Courses[i].hours} hours ${Courses[i].min} minutes</p>
+            <p><i class="fa-solid fa-file-invoice-dollar"></i> ${Courses[i].Price} $</p>
+            <hr>
+            <img src="imgs/teachers/avatar-5-100x100.jpg" style="width:30px; border-radius:50% ;">
+            <h6 style="display:inline-block ;">${Courses[i].Teacher_Name}</h6>
+        </div>
+        </div>
+
+        `
+    }
+    AllCourses.innerHTML = val;
+}
+
+let divs = document.querySelectorAll('.show-course');
+
+for(let i =0;i<Courses.length;i++) {
+
+    divs[i].addEventListener('click', ()=> {
+        pass(i);
+    })
+}
+// passing data to product html page
+function pass(id)
+{
+    localStorage.setItem('id',id);
+    window.location.href = 'COURSE.html';
+}
---
+++ Knowledge Academy/Knowledge Academy/js/COURSE.js
@@ -0,0 +1,2 @@
+let id = localStorage.getItem('id');
+document.getElementById('h1').innerHTML = id;
